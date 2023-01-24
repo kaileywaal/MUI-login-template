4aeff42106ba09a070f194de577d58ca427750aa
Kailey Waal <15kwaal@gmail.com>
2023-01-23 21:33:09 -0700

    fixed signup form

--- kozukai-react-app/src/components/App.js
+++ kozukai-react-app/src/components/App.js
@@ -1,11 +1,11 @@
-import React from "react"
-import Signup from "./Signup"
-import "bootstrap/dist/css/bootstrap.min.css"
-import { Container } from "react-bootstrap"
+import React from "react";
+import Signup from "./Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "@mui/material";
 import { AuthProvider } from "../contexts/AuthContext";
-import { Route, Routes } from "react-router-dom"
-import Dashboard from './Dashboard'
-import Login from './Login'
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import ForgotPassword from "./ForgotPassword";
 import UpdateProfile from "./UpdateProfile";
@@ -13,14 +13,36 @@ import UpdateProfile from "./UpdateProfile";
 function App() {
   return (
     <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
-        <div className="w-100" style={{maxWidth: "400px"}}>
+      <Container
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
           <Routes>
-            <Route exact path="/" element={<PrivateRoute><Dashboard/></PrivateRoute>}/> 
-            <Route path="/update-profile" element={<PrivateRoute><UpdateProfile/></PrivateRoute>}/>
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/forgot-password" element={<ForgotPassword/>}/>
+            <Route
+              exact
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/update-profile"
+              element={
+                <PrivateRoute>
+                  <UpdateProfile />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
           </Routes>
         </div>
       </Container>
--- kozukai-react-app/src/components/Signup.js
+++ kozukai-react-app/src/components/Signup.js
@@ -1,57 +1,108 @@
-import React, { useRef, useState } from 'react'
-import { Typography, Alert, Card, Box, TextField, Button } from '@mui/material'
-import { useAuth } from "../contexts/AuthContext"
-import { Link, useNavigate } from "react-router-dom"
-import { styled } from '@mui/material/styles';
-import { CardContent } from '@mui/material';
+import React, { useState } from "react";
+import { Typography, Alert, Card, Box, TextField, Button } from "@mui/material";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import { CardContent } from "@mui/material";
 
 export default function Signup() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
-  const { signup } = useAuth()
-  const [ error, setError ] = useState("")
-  const [ loading, setLoading ] = useState(false)
-  const navigation = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigate();
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match")
+    if (password !== passwordConfirm) {
+      return setError("Passwords do not match");
     }
     try {
-      setError("")
-      setLoading(true)
-      await signup(emailRef.current.value, passwordRef.current.value)
-      navigation("/")
+      setError("");
+      setLoading(true);
+      await signup(email, password);
+      navigation("/");
     } catch {
-      setError('Failed to create an account')
+      setError("Failed to create an account");
     }
 
-    setLoading(false)
+    setLoading(false);
   }
 
-  const CustomTextField = styled(TextField)({
-    width: "100%",
-    marginBottom: 10
-  })
+  const CustomTextField = styled(TextField)({});
+
+  const handleEmailChange = (event) => {
+    event.preventDefault();
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    event.preventDefault();
+    setPassword(event.target.value);
+  };
+
+  const handlePasswordConfirmChange = (event) => {
+    event.preventDefault();
+    setPasswordConfirm(event.target.value);
+  };
 
   return (
     <React.Fragment>
       <Card>
         <CardContent>
-          <Typography variant="h2" sx={{textAlign: "center"}}>Sign Up</Typography>
-          {error && <Alert severity="error">{error}</Alert>}
+          <Typography variant="h2" sx={{ textAlign: "center" }}>
+            Sign Up
+          </Typography>
+          {error && (
+            <Alert severity="error" sx={{ marginBottom: "10px" }}>
+              {error}
+            </Alert>
+          )}
           <Box component="form" onSubmit={handleSubmit}>
-            <CustomTextField id="email" label="Email" type="email" required ref={emailRef} />
-            <CustomTextField id="password" label="Password" type="password" required ref={passwordRef} />
-            <CustomTextField id="password=confirm" label="Confirm Password" type="password" required ref={passwordConfirmRef} />
-            <Button variant="contained" disabled={loading} type="submit" sx={{width: "100%"}}>Sign Up</Button>
+            <TextField
+              id="email"
+              label="Email"
+              type="email"
+              required
+              value={email}
+              onChange={handleEmailChange}
+              sx={{ width: "100%", marginBottom: "10px" }}
+            />
+            <TextField
+              id="password"
+              label="Password"
+              type="password"
+              required
+              value={password}
+              onChange={handlePasswordChange}
+              sx={{ width: "100%", marginBottom: "10px" }}
+            />
+            <TextField
+              id="password-confirm"
+              label="Confirm Password"
+              type="password"
+              required
+              value={passwordConfirm}
+              onChange={handlePasswordConfirmChange}
+              sx={{ width: "100%", marginBottom: "10px" }}
+            />
+            <Button
+              variant="contained"
+              disabled={loading}
+              type="submit"
+              sx={{ width: "100%" }}
+            >
+              Sign Up
+            </Button>
           </Box>
         </CardContent>
       </Card>
-      <div>Already have an account? <Link to="/login">Log in</Link></div>
+      <div>
+        Already have an account? <Link to="/login">Log in</Link>
+      </div>
     </React.Fragment>
-  )
+  );
 }
